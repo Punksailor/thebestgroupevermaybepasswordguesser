1adaa5e68fc1e81412d36c0cd679f85e180da956
Punksailor <gomezca1013@gmail.com>
2017-03-01 22:06:54 +0000

    my comment

--- corn.js
+++ corn.js
@@ -1,10 +1,16 @@
 /* global $ */
 $(document).ready(function(){
-var answer = "word"
-
-   $("button").click(function(){
-    //alert("DID YOU JUST ASSUME MY GENDER");
- alert($("#No").val());    
+    var answer = "word";
+    
+    $("button").click(function(){
+        alert("DID YOU JUST ASSUME MY GENDER");
+     
+     
+        if (answer === $("#No").val()){
+            alert("you did good boi");
+        } else {
+            alert("nope");
+        }
 
    });
 });
