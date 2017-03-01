8c09801e41b984d52c7ddaf0732f6b6d3b075e15
Punksailor <gomezca1013@gmail.com>
2017-03-01 22:22:38 +0000

    my comment

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# thebestgroupevermaybepasswordguesser
+# The best group ever maybe password guesser
+What our project does is a password guesser. This password guesser is to give self
--- corn.js
+++ corn.js
@@ -1,15 +1,15 @@
 /* global $ */
 $(document).ready(function(){
-    var answer = "word";
+    var answer = "What is your pronoun?";
     
     $("button").click(function(){
         alert("DID YOU JUST ASSUME MY GENDER");
      
      
         if (answer === $("#No").val()){
-            alert("you did good boi");
+            alert("thank you for respecting my pronouns");
         } else {
-            alert("nope");
+            alert("I WILL CALL MY LAWYERS ON YOU!!!");
         }
 
    });
